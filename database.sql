12c13decc517d3cc3ac38d9d200dc95ff459a57f
tshwarelo <tshwarelo@221967575@edu.vut.ac.za>
2023-10-24 22:13:39 +0200

    Committing local changes in database sql

---
+++ database.sql
@@ -0,0 +1,31 @@
+CREATE USER 'tshwarelo'@'%' IDENTIFIED BY 'Ttmokoane7';
+GRANT ALL PRIVILEGES ON *.* TO 'tshwarelo'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+# creating data base named  "student records"
+
+CREATE DATABASE student_records;
+
+# switching to student records
+
+USE student_records;
+
+# creating a table 
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    course VARCHAR(255) NOT NULL
+);
+# sample
+
+INSERT INTO students (name, age, gender, course)
+VALUES
+('Happy Taba', 20, 'Male', 'Computer Science'),
+('Lucas Makua', 22, 'Female', 'Fashion'),
+('Karabo /malome', 19, 'Male', 'Financial Systems');
+
+# diplay
+
+SELECT * FROM students;
